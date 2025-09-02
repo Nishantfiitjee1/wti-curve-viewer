fc51c0b3ea82a70c0deaad6cd31276cecdf8225d
Nishant kumar <96333953+Nishantfiitjee1@users.noreply.github.com>
2025-09-02 14:12:14 +0100

    Update app_Fly.py

--- app_Fly.py
+++ app_Fly.py
@@ -350,6 +350,7 @@ with st.sidebar:
     # The key is the user-friendly name for the dropdown.
     # The value is the actual filename in your folder.
     INBUILT_FILES = {
+        "Jan Fly": "JAN_FLY.xlsx",
         "Feb Fly": "FEB_FLY.xlsx",
         "April Fly": "APRIL_FLY.xlsx",
         "May Fly": "MAY_FLY.xlsx",
